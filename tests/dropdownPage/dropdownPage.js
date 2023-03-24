d2cead5456d39784218abeb44cb354dc71d59dba
jbraden <jbraden@celestialmediagroupllc.com>
2023-03-24 13:41:02 -0500

    User can select multiple options from multi-select

--- tests/dropdownPage/dropdownPage.js
+++ tests/dropdownPage/dropdownPage.js
@@ -5,11 +5,22 @@ import dropdownPageRepo from '../../pom/dropdownPageRepo'
 fixture `letCodeIn - dropdown`
     .page `https://letcode.in/dropdowns`
 
-test('User Can Select Option from Dropdown', async t => {
+test('User Can Select a Single Option from Dropdown', async t => {
     const optionNotification = Selector('p').withText('You have selected Apple')
 
     await dropdownPageRepo.selectOption('Apple')
 
     await t
         .expect(optionNotification.visible).ok()
+})
+
+test('User Can Select Multiple Options from Dropdown', async t => {
+    const option1 = Selector('#superheros').find('option').withText('The Avengers')
+    const option2 = Selector('#superheros').find('option').withText('Batman')
+
+    await dropdownPageRepo.multiSelectOptions(option1, option2)
+
+    await t
+        .expect(option1.selected).ok()
+        .expect(option2.selected).ok()
 })

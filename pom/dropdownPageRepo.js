5ffda3d4697102a17e010fa97784f3288d7a4c1f
jbraden <jbraden@celestialmediagroupllc.com>
2023-03-24 11:48:21 -0500

    User can select an option from dropdown

--- pom/dropdownPageRepo.js
+++ pom/dropdownPageRepo.js
@@ -1,9 +1,16 @@
-import { Selector } from 'testcafe'
+import { Selector, t } from 'testcafe'
 
 
 class dropdownPageRepo {
     constructor() {
         this.h1Dropdown = Selector('h1').withText('Dropdown')
+        this.selectFruits = Selector('#fruits')
+    }
+
+    async selectOption(option) {
+        await t
+            .click(this.selectFruits)
+            .click(Selector('option').withText(option))
     }
 }
 

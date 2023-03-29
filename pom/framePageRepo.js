e64e94dda245c26770772f0948795cc9fc600481
jbraden <jbraden@celestialmediagroupllc.com>
2023-03-29 13:54:51 -0500

    User Can Populate Fields Inside IFrames

--- pom/framePageRepo.js
+++ pom/framePageRepo.js
@@ -1,9 +1,23 @@
-import { Selector } from 'testcafe'
+import { Selector, t } from 'testcafe'
 
 
 class framePageRepo {
     constructor() {
         this.h1Frame = Selector('h1').withText('Frame')
+        this.iframeFirst = Selector('#firstFr')
+        this.txtFirstName = Selector('input').withAttribute('name', 'fname')
+        this.txtLastName = Selector('input').withAttribute('name', 'lname')
+    }
+
+    async switchToIFrame(iFrame) {
+        await t
+            .switchToIframe(this.iframeFirst)
+    }
+
+    async populateNameFields(firstName, lastName) {
+        await t
+            .typeText(this.txtFirstName, firstName, { replace: true, paste: true})
+            .typeText(this.txtLastName, lastName, { replace: true, paste: true})
     }
 }
 
